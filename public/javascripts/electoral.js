6e6a4b6f80847e9d46163ddaa0d739d07bd12a2e
Arie.Prasetiyo <arie>
2019-04-23 19:36:06 +0700

    update

--- public/javascripts/electoral.js
+++ public/javascripts/electoral.js
@@ -1,21 +1,132 @@
 var app = new Vue({
   el: '#app',
+  components: {
+    apexchart: VueApexCharts,
+  },
   data() {
     return {
       info: null,
       hasil: null,
-      dapil: null
+      dapil: null,
+      series: [{
+        name: 'Perolehan Suara',
+        data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+      }],
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: 'bar',
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              position: 'top', // top, center, bottom
+            },
+          }
+        },
+        dataLabels: {
+          enabled: true,
+          formatter: function(val) {
+            return val + "%";
+          },
+          offsetY: -20,
+          style: {
+            fontSize: '12px',
+            colors: ["#304758"]
+          }
+        },
+
+        xaxis: {
+          categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+          position: 'bottom',
+          labels: {
+            offsetY: -18,
+
+          },
+          axisBorder: {
+            show: false
+          },
+          axisTicks: {
+            show: false
+          },
+          crosshairs: {
+            fill: {
+              type: 'gradient',
+              gradient: {
+                colorFrom: '#D8E3F0',
+                colorTo: '#BED1E6',
+                stops: [0, 100],
+                opacityFrom: 0.4,
+                opacityTo: 0.5,
+              }
+            }
+          },
+          tooltip: {
+            enabled: true,
+            offsetY: -35,
+
+          }
+        },
+        fill: {
+          gradient: {
+            shade: 'light',
+            type: "horizontal",
+            shadeIntensity: 0.25,
+            gradientToColors: undefined,
+            inverseColors: true,
+            opacityFrom: 1,
+            opacityTo: 1,
+            stops: [50, 0, 100, 100]
+          },
+        },
+        yaxis: {
+          axisBorder: {
+            show: false
+          },
+          axisTicks: {
+            show: false,
+          },
+          labels: {
+            show: false,
+            formatter: function(val) {
+              return val + "%";
+            }
+          }
+
+        },
+        title: {
+          text: 'Total Perolehan Suara Partai',
+          floating: true,
+          offsetY: 320,
+          align: 'center',
+          style: {
+            color: '#444'
+          }
+        }
+      }
     }
   },
   mounted() {
+    var info;
     axios
       .get('/legislatif/partai')
-      .then(response => (this.info = response.data))
-
+      .then(response => {
+        this.info = response.data
+        for (var i = 0; i < response.data.length; i++) {
+          console.log(response.data[i])
+        }
+        var partai = [];
+        var warna = [];
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
     axios
       .get('/legislatif/hasil')
       .then(response => (this.hasil = response.data))
-      
+
     axios
       .get('/legislatif/dapil')
       .then(response => (this.dapil = response.data))
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const client = redis.createClient();
 var current_page = 0;
 var KP = 'https://kawal-c1.appspot.com/api/c/' + current_page + '?' + new Date().getTime()
 var KPU = 'https://pemilu2019.kpu.go.id/static/json/hhcw/ppwp.json'
+var KP_ONNO = 'https://pantau.kawalpilpres2019.id/api/tps.json?nocache=' + new Date().getTime()
 var KPU_DET = 'https://pemilu2019.kpu.go.id/static/json/hhcw/ppwp/' + current_page + '.json'
 const request = require('request');
 const numeral = require('numeral');
 const async = require('async');
+
 /* GET home page. */
 router.get('/', cached, getInformation);
 
@@ -29,6 +31,17 @@ function render(results, req, res) {
   var kpjs1 = 0;
   var kpjs2 = 0;
   var total_cakupan = 0;
+
+  var kp_onno_cakupan = 0;
+  var kp_onno_pas1 = 0;
+  var kp_onno_pas2 = 0;
+  //console.log(Object.values(results.KP_ONNO))
+  for (var i = 0, len = Object.values(results.KP_ONNO).length; i < len; i++) {
+    kp_onno_cakupan = kp_onno_cakupan + parseInt(Object.values(results.KP_ONNO)[i]['tps_masuk'])
+    kp_onno_pas1 = kp_onno_pas1 + parseInt(Object.values(results.KP_ONNO)[i]['jokowi_amin'])
+    kp_onno_pas2 = kp_onno_pas2 + parseInt(Object.values(results.KP_ONNO)[i]['prabowo_sandi'])
+  }
+
   for (var i = 0, len = results.KP.children.length; i < len; i++) {
     total_cakupan = total_cakupan + parseInt(results.KP.data[results.KP.children[i][0]]['sum']['cakupan'])
     kpjs1 = kpjs1 + parseInt(results.KP.data[results.KP.children[i][0]]['sum']['pas1'])
@@ -40,6 +53,9 @@ function render(results, req, res) {
       title: KPU,
       data: results,
       kpjs1: kpjs1,
+      kpp1: kp_onno_pas1,
+      kpp2: kp_onno_pas2,
+      kppcakupan: kp_onno_cakupan,
       kpjs2: kpjs2,
       total_cakupan: total_cakupan,
       numeral: numeral
@@ -49,6 +65,9 @@ function render(results, req, res) {
     title: KPU,
     data: results,
     kpjs1: kpjs1,
+    kpp1: kp_onno_pas1,
+    kpp2: kp_onno_pas2,
+    kppcakupan: kp_onno_cakupan,
     kpjs2: kpjs2,
     total_cakupan: total_cakupan,
     numeral: numeral
@@ -70,13 +89,35 @@ function getInformation(req, res) {
           }
         });
       },
+      KP_ONNO: function(callback) {
+        request(KP_ONNO, {
+          json: true,
+          strictSSL: false
+        }, (err, res, body) => {
+          if (err) {
+            //console.log(err);
+            callback(err, null);
+          } else {
+            const obj = {};
+            for (const data of body) {
+              var id = data.id;
+              if (data.id === "9000000") {
+                id = -99
+              }
+              obj[id] = data;
+            }
+            //console.log(obj)
+            callback(null, obj);
+          }
+        });
+      },
       KPU: function(callback) {
         request(KPU_URL, {
           json: true,
           strictSSL: false
         }, (err, res, body) => {
           if (err) {
-            console.log(err);
+            //console.log(err);
             callback(err, null);
           } else {
             callback(null, body);
@@ -85,6 +126,7 @@ function getInformation(req, res) {
       }
     },
     function(err, results) {
+      //console.log(results);
       if (!err) {
         client.setex(current_page, 600, JSON.stringify(results));
       }
