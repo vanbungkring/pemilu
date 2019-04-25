1d6c0ccb1bc899070074016919bb779d522823f0
Arie.Prasetiyo <arie>
2019-04-26 00:30:07 +0700

    update index

--- routes/electoral.js
+++ routes/electoral.js
@@ -5,39 +5,56 @@ var DAPIL = 'https://pemilu2019.kpu.go.id/static/json/dapil/dprri.json?' + new D
 var HASIL = 'https://pemilu2019.kpu.go.id/static/json/hhcd/0.json'
 const request = require('request');
 const numeral = require('numeral');
+var key = 'electoral'
 const async = require('async');
+const redis = require('redis');
+const client = redis.createClient();
 const DAPIL_STATIC = require('../static/dapil');
 const PARTAI_STATIC = require('../static/partai');
 router.get('/', calculate);
 router.get('/dapil', generatedapil);
-router.get('/hasil', generateHasil);
+router.get('/hasil',cached, generateHasil);
 router.get('/partai', generatePartai);
 
-function generatedapil(req, res) {
-  res.json(DAPIL_STATIC);
-}
+function cached(req, res, next) {
+  client.get(key, function(err, data) {
+      if (err) {
+        next();
+      };
+      if (data != null) {
+        return res.json(JSON.parse(data));
+        }
+        else {
+          next();
+        }
+      });
+  }
 
-function generatePartai(req, res) {
-  res.json(PARTAI_STATIC);
-}
+  function generatedapil(req, res) {
+    res.json(DAPIL_STATIC);
+  }
 
-function generateHasil(req, res) {
-  request(HASIL, {
-    json: true,
-    strictSSL: false
-  }, (err, response, body) => {
-    if (err) {
-      res.json(err)
-    } else {
-      console.log(body);
-      res.json(body)
-    }
-  });
-}
+  function generatePartai(req, res) {
+    res.json(PARTAI_STATIC);
+  }
 
-function calculate(req, res) {
-  res.render('electoral', {
-    title: 'Simulasi Perolehan Kursi Legislatif'
-  });
-}
-module.exports = router;
+  function generateHasil(req, res) {
+    request(HASIL, {
+      json: true,
+      strictSSL: false
+    }, (err, response, body) => {
+      if (err) {
+        res.json(err)
+      } else {
+        client.setex(key, 7200, JSON.stringify(body));
+        res.json(body)
+      }
+    });
+  }
+
+  function calculate(req, res) {
+    res.render('electoral', {
+      title: 'Simulasi Perolehan Kursi Legislatif'
+    });
+  }
+  module.exports = router;
