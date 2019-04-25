5f4f0b52e5864336b5b17fba45f872830dc1edb1
Arie.Prasetiyo <arie>
2019-04-25 21:56:13 +0700

    update index

---
+++ static/provinsiid.js
@@ -0,0 +1,178 @@
+var WILAYAH = {
+  "1": {
+    "name": "aceh",
+    "id": 1,
+    "seo": "Hasil Real count provinsi ACEH"
+  },
+  "6728": {
+    "name": "sumatera-utara",
+    "id": 6728,
+    "seo": "Hasil Real count provinsi SUMATERA UTARA"
+  },
+  "12920": {
+    "name": "sumatera-barat",
+    "id": 12920,
+    "seo": "Hasil Real count provinsi SUMATERA BARAT"
+  },
+  "14086": {
+    "name": "riau",
+    "id": 14086,
+    "seo": "Hasil Real count provinsi RIAU"
+  },
+  "15885": {
+    "name": "jambi",
+    "id": 15885,
+    "seo": "Hasil Real count provinsi JAMBI"
+  },
+  "17404": {
+    "name": "sumatera-selatan",
+    "id": 17404,
+    "seo": "Hasil Real count provinsi SUMATERA SELATAN"
+  },
+  "20802": {
+    "name": "bengkulu",
+    "id": 20802,
+    "seo": "Hasil Real count provinsi BENGKULU"
+  },
+  "22328": {
+    "name": "lampung",
+    "id": 22328,
+    "seo": "Hasil Real count provinsi LAMPUNG"
+  },
+  "24993": {
+    "name": "kepulauan-bangka-belitung",
+    "id": 24993,
+    "seo": "Hasil Real count provinsi KEPULAUAN BANGKA BELITUNG"
+  },
+  "25405": {
+    "name": "kepulauan-riau",
+    "id": 25405,
+    "seo": "Hasil Real count provinsi KEPULAUAN RIAU"
+  },
+  "25823": {
+    "name": "dki-jakarta",
+    "id": 25823,
+    "seo": "Hasil Real count provinsi DKI JAKARTA"
+  },
+  "26141": {
+    "name": "jawa-barat",
+    "id": 26141,
+    "seo": "Hasil Real count provinsi JAWA BARAT"
+  },
+  "32676": {
+    "name": "jawa-tengah",
+    "id": 32676,
+    "seo": "Hasil Real count provinsi JAWA TENGAH"
+  },
+  "41863": {
+    "name": "daerah-istimewa-yogyakarta",
+    "id": 41863,
+    "seo": "Hasil Real count provinsi DAERAH ISTIMEWA YOGYAKARTA"
+  },
+  "42385": {
+    "name": "jawa-timur",
+    "id": 42385,
+    "seo": "Hasil Real count provinsi JAWA TIMUR"
+  },
+  "51578": {
+    "name": "banten",
+    "id": 51578,
+    "seo": "Hasil Real count provinsi BANTEN"
+  },
+  "53241": {
+    "name": "bali",
+    "id": 53241,
+    "seo": "Hasil Real count provinsi BALI"
+  },
+  "54020": {
+    "name": "nusa-tenggara-barat",
+    "id": 54020,
+    "seo": "Hasil Real count provinsi NUSA TENGGARA BARAT"
+  },
+  "55065": {
+    "name": "nusa-tenggara-timur",
+    "id": 55065,
+    "seo": "Hasil Real count provinsi NUSA TENGGARA TIMUR"
+  },
+  "58285": {
+    "name": "kalimantan-barat",
+    "id": 58285,
+    "seo": "Hasil Real count provinsi KALIMANTAN BARAT"
+  },
+  "60371": {
+    "name": "kalimantan-tengah",
+    "id": 60371,
+    "seo": "Hasil Real count provinsi KALIMANTAN TENGAH"
+  },
+  "61965": {
+    "name": "kalimantan-selatan",
+    "id": 61965,
+    "seo": "Hasil Real count provinsi KALIMANTAN SELATAN"
+  },
+  "64111": {
+    "name": "kalimantan-timur",
+    "id": 64111,
+    "seo": "Hasil Real count provinsi KALIMANTAN TIMUR"
+  },
+  "65702": {
+    "name": "sulawesi-utara",
+    "id": 65702,
+    "seo": "Hasil Real count provinsi SULAWESI UTARA"
+  },
+  "67393": {
+    "name": "sulawesi-tengah",
+    "id": 67393,
+    "seo": "Hasil Real count provinsi SULAWESI TENGAH"
+  },
+  "69268": {
+    "name": "sulawesi-selatan",
+    "id": 69268,
+    "seo": "Hasil Real count provinsi SULAWESI SELATAN"
+  },
+  "72551": {
+    "name": "sulawesi-tenggara",
+    "id": 72551,
+    "seo": "Hasil Real count provinsi SULAWESI TENGGARA"
+  },
+  "74716": {
+    "name": "gorontalo",
+    "id": 74716,
+    "seo": "Hasil Real count provinsi GORONTALO"
+  },
+  "75425": {
+    "name": "sulawesi-barat",
+    "id": 75425,
+    "seo": "Hasil Real count provinsi SULAWESI BARAT"
+  },
+  "76096": {
+    "name": "maluku",
+    "id": 76096,
+    "seo": "Hasil Real count provinsi MALUKU"
+  },
+  "77085": {
+    "name": "maluku-utara",
+    "id": 77085,
+    "seo": "Hasil Real count provinsi MALUKU UTARA"
+  },
+  "78203": {
+    "name": "papua",
+    "id": 78203,
+    "seo": "Hasil Real count provinsi PAPUA"
+  },
+  "81877": {
+    "name": "papua-barat",
+    "id": 81877,
+    "seo": "Hasil Real count provinsi PAPUA BARAT"
+  },
+  "928068": {
+    "name": "kalimantan-utara",
+    "id": 928068,
+    "seo": "Hasil Real count provinsi KALIMANTAN UTARA"
+  },
+  "-99": {
+    "name": "luar-negeri",
+    "id": -99,
+    "seo": "Hasil Real count provinsi Luar Negeri"
+  }
+}
+module.exports = WILAYAH;
