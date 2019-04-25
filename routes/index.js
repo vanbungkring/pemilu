95d12a52eb8aed3be16ac200d65ed0487f4ae0fb
Arie.Prasetiyo <arie>
2019-04-25 14:30:16 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const client = redis.createClient();
 var current_page = 0;
 var KP = 'https://kawal-c1.appspot.com/api/c/' + current_page + '?' + new Date().getTime()
 var KPU = 'https://pemilu2019.kpu.go.id/static/json/hhcw/ppwp.json'
-var KP_ONNO = 'https://pantau.kawalpilpres2019.id/api/tps.json?nocache=' + new Date().getTime()
+var KP_ONNO = 'https://pantau.kawalpilpres2019.id/api/tps.json'
 var KPU_DET = 'https://pemilu2019.kpu.go.id/static/json/hhcw/ppwp/' + current_page + '.json'
 const request = require('request');
 const numeral = require('numeral');
